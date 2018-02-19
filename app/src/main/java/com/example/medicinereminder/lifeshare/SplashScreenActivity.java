b2e7b6d06a42cc5b417baae56a8abededc1e8768
Sukrit-Mehta <sukritmehta1996@gmail.com>
2018-02-20 01:08:25 +0530

    Initial commit.

---
+++ app/src/main/java/com/example/medicinereminder/lifeshare/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.example.medicinereminder.lifeshare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler=new Handler();
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,HomeActivity.class));
+            }
+        };
+        handler.postDelayed(runnable,2000);
+
+    }
+}
